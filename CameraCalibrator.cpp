25a0413efe59b0715498834b0d53a14c506b9d46
venkateshtata <venkateshtata9@gmail.com>
2020-04-30 12:52:06 +0530

    first commit

--- CameraCalibrator.cpp
+++ CameraCalibrator.cpp
@@ -17,14 +17,36 @@
 
 #include "CameraCalibrator.h"
 
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <cstdlib>
+#include <cstdlib>
+#include <random>
+#include <vector>
+
+#include "opencv2/features2d/features2d.hpp"
+
+#include "opencv2/xfeatures2d.hpp"
+
+#include <opencv2/xfeatures2d/nonfree.hpp>
+#include <opencv2/xfeatures2d.hpp>
+
+
+
+using namespace std;
+using namespace cv;
+
+std::vector<cv::Mat> rvecs, tvecs;
+
 // Open chessboard images and extract corner points
 int CameraCalibrator::addChessboardPoints(
          const std::vector<std::string>& filelist, 
          cv::Size & boardSize) {
 
 	// the points on the chessboard
-    std::vector<cv::Point2f> imageCorners;
-    std::vector<cv::Point3f> objectCorners;
+  std::vector<cv::Point2f> imageCorners;
+  std::vector<cv::Point3f> objectCorners;
+    
 
     // 3D Scene Points:
     // Initialize the chessboard corners 
@@ -67,6 +89,8 @@ int CameraCalibrator::addChessboardPoints(
             successes++;
           }
 
+
+
         //Draw the corners
         cv::drawChessboardCorners(image, boardSize, imageCorners, found);
         cv::imshow("Corners on Chessboard", image);
@@ -83,6 +107,7 @@ void CameraCalibrator::addPoints(const std::vector<cv::Point2f>& imageCorners, c
 	imagePoints.push_back(imageCorners);          
 	// corresponding 3D scene points
 	objectPoints.push_back(objectCorners);
+
 }
 
 // Calibrate the camera
@@ -93,12 +118,12 @@ double CameraCalibrator::calibrate(cv::Size &imageSize)
 	mustInitUndistort= true;
 
 	//Output rotations and translations
-    std::vector<cv::Mat> rvecs, tvecs;
+    
 
 	// start calibration
 	return 
      calibrateCamera(objectPoints, // the 3D points
-		            imagePoints,  // the image points
+		      imagePoints,  // the image points
 					imageSize,    // image size
 					cameraMatrix, // output camera matrix
 					distCoeffs,   // output distortion matrix
@@ -145,4 +170,187 @@ void CameraCalibrator::setCalibrationFlag(bool radial8CoeffEnabled, bool tangent
     flag = 0;
     if (!tangentialParamEnabled) flag += CV_CALIB_ZERO_TANGENT_DIST;
 	if (radial8CoeffEnabled) flag += CV_CALIB_RATIONAL_MODEL;
+}
+
+
+cv::Vec3d CameraCalibrator::triangulate(const cv::Mat &p1, const cv::Mat &p2, const cv::Vec2d &u1, const cv::Vec2d &u2) {
+
+  // system of equations assuming image=[u,v] and X=[x,y,z,1]
+  // from u(p3.X)= p1.X and v(p3.X)=p2.X
+  cv::Matx43d A(u1(0)*p1.at<double>(2, 0) - p1.at<double>(0, 0),
+  u1(0)*p1.at<double>(2, 1) - p1.at<double>(0, 1),
+  u1(0)*p1.at<double>(2, 2) - p1.at<double>(0, 2),
+  u1(1)*p1.at<double>(2, 0) - p1.at<double>(1, 0),
+  u1(1)*p1.at<double>(2, 1) - p1.at<double>(1, 1),
+  u1(1)*p1.at<double>(2, 2) - p1.at<double>(1, 2),
+  u2(0)*p2.at<double>(2, 0) - p2.at<double>(0, 0),
+  u2(0)*p2.at<double>(2, 1) - p2.at<double>(0, 1),
+  u2(0)*p2.at<double>(2, 2) - p2.at<double>(0, 2),
+  u2(1)*p2.at<double>(2, 0) - p2.at<double>(1, 0),
+  u2(1)*p2.at<double>(2, 1) - p2.at<double>(1, 1),
+  u2(1)*p2.at<double>(2, 2) - p2.at<double>(1, 2));
+
+  cv::Matx41d B(p1.at<double>(0, 3) - u1(0)*p1.at<double>(2,3),
+                p1.at<double>(1, 3) - u1(1)*p1.at<double>(2,3),
+                p2.at<double>(0, 3) - u2(0)*p2.at<double>(2,3),
+                p2.at<double>(1, 3) - u2(1)*p2.at<double>(2,3));
+
+  // X contains the 3D coordinate of the reconstructed point
+  cv::Vec3d X;
+  // solve AX=B
+  cv::solve(A, B, X, cv::DECOMP_SVD);
+  return X;
+}
+
+// triangulate a vector of image points
+void CameraCalibrator::triangulate(const cv::Mat &p1, const cv::Mat &p2, const std::vector<cv::Vec2d> &pts1, const std::vector<cv::Vec2d> &pts2, std::vector<cv::Vec3d> &pts3D) {
+
+  for (int i = 0; i < pts1.size(); i++) {
+
+    pts3D.push_back(triangulate(p1, p2, pts1[i], pts2[i]));
+  }
+}
+
+
+
+
+int main(){
+
+
+
+
+  cout<<"compiled"<<endl;
+
+  const std::vector<std::string> files = {"chess1.jpg", "chess2.jpg"};
+  cv::Size board_size(7,7);
+
+  CameraCalibrator cal;
+  cal.addChessboardPoints(files, board_size);
+
+  cv::Mat img = cv::imread("chess1.jpg");
+
+  cv::Size img_size = img.size();
+  cal.calibrate(img_size);
+  cout<<cameraMatrix<<endl;
+
+
+  cv::Mat image1 = cv::imread("m3.jpg");
+  cv::Mat image2 = cv::imread("m4.jpg");
+
+  // vector of keypoints and descriptors
+  std::vector<cv::KeyPoint> keypoints1;
+  std::vector<cv::KeyPoint> keypoints2;
+  cv::Mat descriptors1, descriptors2;
+
+  // Construction of the SIFT feature detector
+  cv::Ptr<cv::Feature2D> ptrFeature2D = cv::xfeatures2d::SIFT::create(500);
+
+  // Detection of the SIFT features and associated descriptors
+  ptrFeature2D->detectAndCompute(image1, cv::noArray(), keypoints1, descriptors1);
+  ptrFeature2D->detectAndCompute(image2, cv::noArray(), keypoints2, descriptors2);
+
+  // Match the two image descriptors
+  // Construction of the matcher with crosscheck
+  cv::BFMatcher matcher(cv::NORM_L2, true);
+  std::vector<cv::DMatch> matches;
+  matcher.match(descriptors1, descriptors2, matches);
+
+  // Convert keypoints into Point2f
+  std::vector<cv::Point2f> points1, points2;
+
+  for (std::vector<cv::DMatch>::const_iterator it = matches.begin(); it != matches.end(); ++it) {
+    // Get the position of left keypoints
+    float x = keypoints1[it->queryIdx].pt.x;
+    float y = keypoints1[it->queryIdx].pt.y;
+    points1.push_back(cv::Point2f(x, y));
+    // Get the position of right keypoints
+    x = keypoints2[it->trainIdx].pt.x;
+    y = keypoints2[it->trainIdx].pt.y;
+    points2.push_back(cv::Point2f(x, y));
+  }
+
+  // Find the essential between image 1 and image 2
+  cv::Mat inliers;
+  cv::Mat essential = cv::findEssentialMat(points1, points2, cameraMatrix, cv::RANSAC, 0.9, 1.0, inliers);
+
+  cout<<essential<<endl;
+
+  // recover relative camera pose from essential matrix
+  cv::Mat rotation, translation;
+  cv::recoverPose(essential, points1, points2, cameraMatrix, rotation, translation, inliers);
+  cout<<rotation<<endl;
+  cout<<translation<<endl;
+
+
+  // compose projection matrix from R,T
+  cv::Mat projection2(3, 4, CV_64F); // the 3x4 projection matrix
+  rotation.copyTo(projection2(cv::Rect(0, 0, 3, 3)));
+  translation.copyTo(projection2.colRange(3, 4));
+  // compose generic projection matrix
+  cv::Mat projection1(3, 4, CV_64F, 0.); // the 3x4 projection matrix
+  cv::Mat diag(cv::Mat::eye(3, 3, CV_64F));
+  diag.copyTo(projection1(cv::Rect(0, 0, 3, 3)));
+  // to contain the inliers
+  std::vector<cv::Vec2d> inlierPts1;
+  std::vector<cv::Vec2d> inlierPts2;
+  // create inliers input point vector for triangulation
+  int j(0);
+  for (int i = 0; i < inliers.rows; i++) {
+    if (inliers.at<uchar>(i)) {
+      inlierPts1.push_back(cv::Vec2d(points1[i].x, points1[i].y));
+      inlierPts2.push_back(cv::Vec2d(points2[i].x, points2[i].y));
+    }
+  }
+  // undistort and normalize the image points
+  std::vector<cv::Vec2d> points1u;
+  cv::undistortPoints(inlierPts1, points1u, cameraMatrix, distCoeffs);
+  std::vector<cv::Vec2d> points2u;
+  cv::undistortPoints(inlierPts2, points2u, cameraMatrix, distCoeffs);
+  
+
+  // Triangulation
+  std::vector<cv::Vec3d> points3D;
+  cal.triangulate(projection1, projection2, points1u, points2u, points3D);
+
+  cout<<"3D points :"<<points3D.size()<<endl;
+
+  viz::Viz3d window; //creating a Viz window
+
+  // viz::WCameraPosition cam(cameraMatrix, image1, 30.0);
+
+
+ 
+
+  
+
+
+
+  //Displaying the Coordinate Origin (0,0,0)
+  window.showWidget("coordinate", viz::WCoordinateSystem());
+  //Displaying the 3D points in green
+  // cv::viz::WCameraPosition camm(cameraMatrix, image1, 30.0, viz::Color::black());
+
+  // window.showWidget("Camera", camm);
+  
+  window.setBackgroundColor(cv::viz::Color::black());
+
+
+  // cv::Mat rvec, tvec;
+  // cv::solvePnP(objectPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec);
+  // cv::Mat rot;
+  // cv::Rodrigues(rvec, rot);
+
+
+  // cv::Affine3d pose(rot, tvec);
+  // window.setWidgetPose("top", pose);
+
+
+
+  window.showWidget("points", viz::WCloud(points3D, viz::Color::green()));
+  window.spin();
+
+
+
+
+
 }
--- CameraCalibrator.h
+++ CameraCalibrator.h
@@ -26,23 +26,31 @@
 #include "opencv2/calib3d/calib3d.hpp"
 #include <opencv2/highgui/highgui.hpp>
 
+std::vector<std::vector<cv::Point3f>> objectPoints;
+std::vector<std::vector<cv::Point2f>> imagePoints;
+// output Matrices
+cv::Mat cameraMatrix;
+cv::Mat distCoeffs;
+
+
+
 class CameraCalibrator {
 
 	// input points
-    std::vector<std::vector<cv::Point3f>> objectPoints;
-    std::vector<std::vector<cv::Point2f>> imagePoints;
-    // output Matrices
-    cv::Mat cameraMatrix;
-    cv::Mat distCoeffs;
+    
 	// flag to specify how calibration is done
 	int flag;
 	// used in image undistortion 
     cv::Mat map1,map2; 
 	bool mustInitUndistort;
+  
 
   public:
 	CameraCalibrator() : flag(0), mustInitUndistort(true) {};
 
+
+  cv::Vec3d triangulate(const cv::Mat &p1, const cv::Mat &p2, const cv::Vec2d &u1, const cv::Vec2d &u2);
+  void triangulate(const cv::Mat &p1, const cv::Mat &p2, const std::vector<cv::Vec2d> &pts1, const std::vector<cv::Vec2d> &pts2, std::vector<cv::Vec3d> &pts3D);
 	// Open the chessboard images and extract corner points
 	int addChessboardPoints(const std::vector<std::string>& filelist, cv::Size & boardSize);
 	// Add scene points and corresponding image points
